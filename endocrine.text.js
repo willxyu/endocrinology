30c3b50c20d2c7fe54911dffaeb5678a60a14e53
william <willxyu@users.noreply.github.com>
2025-03-19 13:29:11 +1100

    Create endocrine.text.js

---
+++ endocrine.text.js
@@ -0,0 +1,23 @@
+endocrinology.explain = {
+  hammersmith: {
+    description: ``,
+    formulae   : ``,
+  },
+  phosphaturia: {
+    description: ``,
+    formulae   : ``,
+  },
+}
+    hammersmith  : hammersmith,
+    phosphaturia : phosphaturia,
+    eag          : eag,
+    homaIR       : homaIR,
+    mg2mmol      : mg2mmol,
+    mmol2mg      : mmol2mg,
+    calcOsm      : calcOsm,
+    correctSodium: correctSodium,
+    bmi          : bmi,
+    bodyfat      : bodyfat,
+    rmrHarris    : rmrHarris,
+    rmrMifflin   : rmrMifflin,
+    bsa          : bsa,
