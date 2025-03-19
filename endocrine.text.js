dabc3667a00dd8cb9ab4d0ebafc4c0c456d941ff
william <willxyu@users.noreply.github.com>
2025-03-19 14:52:29 +1100

    Update endocrine.text.js

--- endocrine.text.js
+++ endocrine.text.js
@@ -7,17 +7,48 @@ endocrinology.explain = {
     description: ``,
     formulae   : ``,
   },
+  eag: {
+    description: ``,
+    formulae   : ``,
+  },
+  homaIR: {
+    description: ``,
+    formulae   : ``,
+  },
+  mg2mmol: {
+    description: ``,
+    formulae   : ``,
+  },
+  mmol2mg: {
+    description: ``,
+    formulae   : ``,
+  },
+  calcOsm: {
+    description: ``,
+    formulae   : ``,
+  },
+  correctSodium: {
+    description: ``,
+    formulae   : ``,
+  },
+  bmi: {
+    description: ``,
+    formulae   : ``,
+  },
+  bodyfat: {
+    description: ``,
+    formulae   : ``,
+  },
+  rmrHarris: {
+    description: ``,
+    formulae   : ``,
+  },
+  rmrMifflin: {
+    description: ``,
+    formulae   : ``,
+  },
+  bsa: {
+    description: ``,
+    formulae   : ``,
+  },
 }
-    hammersmith  : hammersmith,
-    phosphaturia : phosphaturia,
-    eag          : eag,
-    homaIR       : homaIR,
-    mg2mmol      : mg2mmol,
-    mmol2mg      : mmol2mg,
-    calcOsm      : calcOsm,
-    correctSodium: correctSodium,
-    bmi          : bmi,
-    bodyfat      : bodyfat,
-    rmrHarris    : rmrHarris,
-    rmrMifflin   : rmrMifflin,
-    bsa          : bsa,
